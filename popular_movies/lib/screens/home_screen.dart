782f984a4385225e4b7db58fdb0abb5b217fb049
krism01@naver.com <krism01@naver.com>
2024-02-23 23:52:34 +0900

    기본 토대 30%

--- popular_movies/lib/screens/home_screen.dart
+++ popular_movies/lib/screens/home_screen.dart
@@ -5,8 +5,62 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('home screen'),
+    TextStyle subjectText = TextStyle(fontWeight: FontWeight.bold, fontSize: 22);
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // note: popular movies
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('Popular Movies', style: subjectText),
+                    ),
+                    Container(
+                      width: 200,
+                      child: Image.asset('assets/images/images.jpeg'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15),
+                // note: now in cinemas
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('Now in Cinemas', style: subjectText),
+                    ),
+                    Container(
+                      width: 160,
+                      child: Image.asset('assets/images/images.jpeg'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15),
+                // note: coming soon
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('Coming soon', style: subjectText),
+                    ),
+                    Container(
+                      width: 160,
+                      child: Image.asset('assets/images/images.jpeg'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
